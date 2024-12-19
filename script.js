42d0be2a6122dac9c439dca8f6792fd8002419cd
tyoma <xtemalitv@gmail.com>
2024-12-19 13:57:04 +0100

    first commit

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('.btnLogin-popup');
 const iconClose = document.querySelector('.icon-close');
-
+ 
 
 registerLink.addEventListener('click', ()=>{
     wrapper.classList.add('active');
